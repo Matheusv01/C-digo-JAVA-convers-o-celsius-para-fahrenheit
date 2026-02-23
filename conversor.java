fc2d6e7a9aa766b71081a5f97c94c4d7cc23f66b
Matheus Vinicius S Chaves <matheusxavesv@gmail.com>
2026-02-23 08:10:49 -0300

    primeiro código java

---
+++ conversor.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    
+        
+    public static void main(String[] args) {
+         
+         
+         //entrada 
+         
+         Scanner teclado = new Scanner(System.in);
+         System.out.println ("digite o valor de c: ");
+         float temperatura_c = teclado.nextFloat();
+         
+         
+         //processamento
+         
+        float temperatura_f = ((temperatura_c * 9 ) / 5) + 32;
+         
+         
+         //saida 
+         
+         
+         System.out.println("o valor de celsius é: " + temperatura_c);
+         System.out.println("o valoR de farenheit  é: " + temperatura_f);
+    }
+}
